a31433d633e69a2e27512e71c83310456078e1b9
Danielle Voznyy <dan.voznyy@gmail.com>
2024-01-27 18:36:13 -0500

    Initial Commit

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,9 @@
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+set EDITOR micro
+
+# eval (/run/host/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+fish_add_path /var/home/offz/.local/bin
+
+starship init fish | source
