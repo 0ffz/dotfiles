1c312c5034b5ee6830f7df4952360f8cbedb01ad
Danielle Voznyy <dan.voznyy@gmail.com>
2025-03-28 14:17:36 -0400

    chore: update homemanager config for laptop

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -1,11 +1,14 @@
 { config, pkgs, ... }:
 
+let
+  pkgs-unstable = import <nixpkgs-unstable> {};
+in
 {
   home.username = "offz";
-  home.homeDirectory = "/var/home/offz";
-  home.stateVersion = "24.05";
+  home.homeDirectory = "/home/offz";
+  home.stateVersion = "24.11";
 
-  home.packages = with pkgs; [
+  home.packages = (with pkgs; [
     starship
     micro
     eza
@@ -14,7 +17,23 @@
     lazydocker
     go-task
     tectonic
-  ];
+    fish
+    jetbrains-mono
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+    jetbrains-toolbox
+    nixd
+    nil
+    graalvm-ce
+    # Gnome extensions
+    gnomeExtensions.middle-click-to-close-in-overview
+    gnomeExtensions.just-perfection
+  ]) ++ (with pkgs-unstable; [
+  	zed-editor
+  ]);
+
+  nixpkgs.config = {
+      allowUnfree = true;
+  };
 
   programs.git = {
     enable = true;
@@ -29,8 +48,32 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-#  programs.zed-editor = {
-#   enable = true;
-#   extensions = [ "nix" ];
-#  };
+
+  # Add nix applications to Gnome app menu
+  xdg.systemDirs.data = [ "${config.home.homeDirectory}/.nix-profile/share/applications" ];
+
+  # Gnome preferences
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      enable-hot-corners = false;
+      clock-format = "12h";
+      text-scaling-factor = 1.5;
+    };
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+    };
+    # Set console font preference
+    "org/gnome/Console" = {
+      custom-font = "Jetbrains Mono 12";
+      use-system-font = false;
+    };
+    # Swap alt-tab to show window previews instead of icons
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-applications = [];
+      switch-applications-backward = [];
+      switch-windows = ["<Alt>Tab"];
+      switch-windows-backward = ["<Shift><Alt>Tab"];
+    };
+  };
 }
