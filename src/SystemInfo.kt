e47bb87c1be913c481026d7db35c1a5671704ebd
Danielle Voznyy <dan.voznyy@gmail.com>
2025-05-01 16:20:55 -0400

    fix: Get release via /etc/os-release

--- src/SystemInfo.kt
+++ src/SystemInfo.kt
@@ -2,7 +2,11 @@ import com.lordcodes.turtle.shellRun
 import util.OperatingSystem
 
 object SystemInfo {
-    val OS = when(val os = shellRun("lsb_release", listOf("-is")).lowercase()) {
+    val OS = when(val os = shellRun("cat", listOf("/etc/os-release"))
+        .lineSequence()
+        .first { it.startsWith("ID=") }
+        .substringAfter("=").trim('"')
+    ) {
         "fedora" -> OperatingSystem.FEDORA
         "arch" -> OperatingSystem.ARCH
         else -> error("Unknown OS: $os")
